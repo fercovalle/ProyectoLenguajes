19b1ffdf9c406b5cf8839f007b7e5ffa2795e005
Fernando Ovalle <carloscoronado201932190@cunoc.edu.gt>
2023-08-19 10:48:02 -0600

    Primer commit en dev

--- src/main/java/Logica/Main.java
+++ src/main/java/Logica/Main.java
@@ -12,7 +12,7 @@ public class Main {
     
     public static void main(String[] args) {
         
-        System.out.println("Test...");
+        System.out.println("Test en branch dev");
     }
     
 }
